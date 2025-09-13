817e06e010045116b674eb61f3b5ff527a3944e6
RUDRANSHFLY <mayamayavir@gmail.com>
2025-09-13 11:26:20 +0530

    feat(auth): add JWT authorization and AuthGuard with getUser decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
 import { AuthModule } from './auth/auth.module';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule, AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule, AuthModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
